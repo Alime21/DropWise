6d64e8b8c5942c3be8e82a967fa4152170bf386a
UserAlime <alime3888@gmail.com>
2026-04-23 00:31:40 +0300

    feat:Initial structure with MVVM and  setup core tab view, dashboard UI and mock data models

---
+++ DropWise/DropWise/DropWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropWiseApp.swift
+//  DropWise
+//
+//  Created by Macbook on 23.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct DropWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
